c14c5a8f6305ee10b68a5831a0a65ad14f634a45
Kevin Tran <kevin1997.t@gmail.com>
2018-09-12 17:15:13 +0100

    Start work on registration activity

--- app/src/main/java/kevin/androidhealthtracker/RegisterActivity.java
+++ app/src/main/java/kevin/androidhealthtracker/RegisterActivity.java
@@ -3,43 +3,46 @@ package kevin.androidhealthtracker;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-import android.content.Context;
-import android.content.Intent;
+
+import android.content.CursorLoader;
 import android.content.Loader;
-import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.AsyncTask;
+
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kevin.healthtracker.datamodels.User;
 
 import org.springframework.web.client.RestClientException;
 
-import java.io.IOException;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
 
-import kevin.androidhealthtracker.util.PropertyReader;
+import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
-    private Properties properties;
-    private PropertyReader propertyReader;
-    private Context context;
-    private WebClient client;
+public class RegisterActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -48,30 +51,22 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     // UI references.
     private EditText mEmailView;
     private EditText mPasswordView;
+    private EditText mPasswordConfirmView;
     private View mProgressView;
     private View mLoginFormView;
+    private WebClient client;
 
-    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_register);
         // Set up the login form.
         mEmailView = findViewById(R.id.usernameInput);
         mPasswordView = findViewById(R.id.password);
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
-        context = this;
-        //TODO: Add this as properties
+        mPasswordConfirmView = findViewById(R.id.passwordConfirm);
         client = MainActivity.client;
 
-        try {
-            properties = new PropertyReader(this, properties).getProperties("app.properties");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        mPasswordView = findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -83,7 +78,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        //Authenticate the user with the server
         Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -92,16 +86,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mRegistrationBUtton = findViewById(R.id.email_register_button);
-        mRegistrationBUtton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registrationIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(registrationIntent);
-            }
-        });
-
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
     }
+
+
+
 
     /**
      * Attempts to sign in or register the account specified by the login form.
@@ -109,20 +99,22 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-        boolean cancel = false;
-        View focusView = null;
-        User user = new User();
-        user.setUserName(email);
-        user.setPassword(password);
-
         if (mAuthTask != null) {
             return;
         }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        String passwordConfirm = mPasswordConfirmView.getText().toString();
+        User user = new User();
+
+        boolean cancel = false;
+        View focusView = null;
 
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
@@ -130,6 +122,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             focusView = mPasswordView;
             cancel = true;
         }
+        if (password.equals(passwordConfirm)){
+            user.setUserName(email);
+            user.setPassword(password);
+        }
+
         // Check for a valid email address.
 //        if (TextUtils.isEmpty(email)) {
 //            mEmailView.setError(getString(R.string.error_field_required));
@@ -152,6 +149,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = new UserLoginTask(user);
             mAuthTask.execute((Void) null);
         }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
     }
 
     /**
@@ -190,6 +197,20 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+    }
+
+
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
@@ -204,11 +225,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         @Override
         protected Boolean doInBackground(Void... params) {
             try{
-                user = client.loginUser(user);
+                user = client.registerUser(user);
             }catch (RestClientException e){
-                System.out.println(e.getMessage());
+                Toast error = new Toast(RegisterActivity.this);
+                error.makeText(RegisterActivity.this, e.toString(), Toast.LENGTH_LONG).show();
             }
 
+            // TODO: register the new account here.
             return true;
         }
 
@@ -218,11 +241,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                SharedPreferences.Editor editor = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE).edit();
-                editor.putInt("userId", user.getId());
-                editor.putString("userName", user.getUserName());
-                editor.apply();
-                setResult(RESULT_OK);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
@@ -236,27 +254,5 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-    }
-
-    private boolean isEmailValid(String email) {
-        return email.matches("[a-zA-Z1-9]+@[a-zA-z]+[.][a-zA-Z]+");
-    }
-
-    private boolean isPasswordValid(String password) {
-        return password.length() > 4;
-    }
-
 }
 
